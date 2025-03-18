eff93352414a3ce060e8aea1f61c1cbfeb02b7d0
LukynVoryson <lukas.voracek@infis.cz>
2025-03-18 08:37:19 +0100

    Add files via upload

---
+++ podminky.py
@@ -0,0 +1,13 @@
+print("Ahoj světe")
+cislo = 10
+if cislo > 5:
+ print("Cislo je větší než 5")
+ if cislo > 8:
+  print("Cislo je větší než 8")
+ else:
+  print("Cislo je menší nebo rovno 8")
+else:
+ print("Cislo je menší než 5")
+for i in range(5):
+ print("Toto je krok číslo " + str(i))
+print("Konec kódu")
